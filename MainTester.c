0aad5881ee3d12e92830614d3f2daeb1e439bf45
Tim <timothyk7@gmail.com>
2014-04-01 01:11:45 -0700

    Change display method

--- MainTester.c
+++ MainTester.c
@@ -9,17 +9,16 @@ int main (int argc, char *argv[]) {
     int prompt; //if should prompt the user or not
     prompt = TRUE; //should promt user
     
+    initialize("maze4.txt");
+    int dist [MAP_SIZE][MAP_SIZE]={0}; /*debug*/
+    
     char *str;
     for(DISPLAY_PROMPT; fgets(buff,BUFSIZ, pFile) !=NULL;DISPLAY_PROMPT)
     {
        str = strtok(buff, TOKEN_SEP);
        if(strcmp(str, "exit") == 0)
-          break;
-       initialize("maze4.txt");
-       
-       int dist [MAP_SIZE][MAP_SIZE]={0}; /*debug*/
-       
-       display(TRUE, dist,0,0,'N');
+          break;      
+       displayMaze(TRUE, dist,0,0,'N');
           
     }
     return 0;
--- MapMaker.h
+++ MapMaker.h
@@ -5,12 +5,9 @@
 #define FALSE 0
 #define TRUE 1
 #define TOKEN_SEP " \t\n"
+
 #define PROMPT "> "
 #define DISPLAY_PROMPT ( (prompt != FALSE) ? (void) printf( PROMPT ) : (void)0 )
-
-
-#define WALL_VALUES 0x0000, 0x0001,0x0010,0x0011,0x0100,0x0101,0x0110,0x0111, \
-                    0x1000,0x1001,0x1010,0x1011,0x1100,0x1101,0x1110,0x1111
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -20,16 +17,15 @@
 
 //struct w/ coord and walls for bot
 struct info{
-              int x;
-              int y;
+              unsigned int x;
+              unsigned int y;
               unsigned int wall;
 };
 
 typedef struct info Info;
       
-//methods
 //variables
-Info mazeMap [MAP_SIZE][MAP_SIZE]; //map of maze
+Info mazeMap [MAP_SIZE][MAP_SIZE]; //map of maze from file
 FILE *inFile = 0; //file of maze
 char buff[BUFSIZ]; //store string from file
 char *endPtr; //for errors
@@ -89,20 +85,23 @@ Info getMapInfo(int x, int y)
             {
                 data.wall = number;
                 mazeMap[data.x][data.y] = data;
-                //(void)printf("%d %d| %d", data.x, data.y, data.wall); /*DEBUG*/
-                //(void)printf("\n");
-                //(void)printf("data: %d %d| %d", mazeMap[data.x][data.y].x, mazeMap[data.x][data.y].y, mazeMap[data.x][data.y].wall); /*DEBUG*/
-                //(void)printf("\n");
             }
             x++;
             
         }while((num = strtok(NULL,TOKEN_SEP)) != NULL);
-          
-        //(void)printf("%d", number);
       }
  }
  
-void display(int num, int number[MAP_SIZE][MAP_SIZE], int cX, int cY, char dir)
+/*
+ * Display the map of the full maze
+   param: 
+          1 whether to display floodfill algorithm
+          2 current map of the bot (floodfill numbers)
+          3 current x location
+          4 current y location
+          5 direction
+ */
+void displayMaze(int num, int number[MAP_SIZE][MAP_SIZE], int cX, int cY, char dir)
       {
         (void)printf("\t ");
         int x = 0;
@@ -122,8 +121,6 @@ void display(int num, int number[MAP_SIZE][MAP_SIZE], int cX, int cY, char dir)
           int col = 0;
           for (col =0; col < MAP_SIZE; col++)
           {
-              //System.out.print(mazeMap[row][col] + "\t");
-              //(void)printf("info: %d, %d",mazeMap[row][col].wall,mazeMap[row][col].wall & 0x0010 );
               if(((mazeMap[row][col].wall>>1) & 0x0001) == 0x0001)//if theres a left wall
                 (void)printf("|");
               else
@@ -164,6 +161,79 @@ void display(int num, int number[MAP_SIZE][MAP_SIZE], int cX, int cY, char dir)
           }
           (void)printf("\n");
         }
+      }
+        
+/*
+ * Display the map of the current bot maze
+   param: 
+          1 whether to display floodfill algorithm
+          2 current map of the bot (floodfill numbers)
+          3 current map of bot
+          4 current x location
+          5 current y location
+          6 direction
+ */
+void displayBotMaze(int num, int number[MAP_SIZE][MAP_SIZE], Info map[MAP_SIZE][MAP_SIZE], int cX, int cY, char dir)
+      {
+        (void)printf("\t ");
+        int x = 0;
+        for (x=0; x<MAP_SIZE; x++)
+        {
+            (void)printf("%d",x%10);    
+            (void)printf("     "); //2 orig
+        }
+        (void)printf("\n");
+        
+        
+        int row = 0;
+        for (row = 0; row < MAP_SIZE; row++)
+        {
+          helpTop(row);
+          (void)printf("%d\t",row);
+          int col = 0;
+          for (col =0; col < MAP_SIZE; col++)
+          {
+              if(((mazeMap[row][col].wall>>1) & 0x0001) == 0x0001)//if theres a left wall
+                (void)printf("|");
+              else
+                (void)printf(" ");
+              if(cX == row && cY == col){
+                if(((mazeMap[row][col].wall>>3)&0x0001) == 0x0001)//if theres a bottom wall
+                    (void)printf("_%c%c_",dir,dir);
+                else
+                    (void)printf("_%c%c_",dir,dir);
+              }else if(num){
+                if(((mazeMap[row][col].wall>>3)&0x0001) == 0x0001){//if theres a bottom wall
+                    if(number[row][col]<10)
+                       (void)printf("_%d__",number[row][col]);
+                    else if(number[row][col]<100)
+                       (void)printf("_%d_", number[row][col]);
+                    else
+                       (void)printf("_%d", number[row][col]);
+                }
+                else{
+                    if(number[row][col]<10)
+                       (void)printf(" %d  ", number[row][col]);
+                    else if(number[row][col]<100)
+                       (void)printf(" %d ", number[row][col]);
+                    else
+                       (void)printf(" %d", number[row][col]);
+                }
+              }else{
+                if(((mazeMap[row][col].wall>>3)&0x0001) == 0x0001)//if theres a bottom wall
+                    (void)printf("____");
+                else
+                    (void)printf("    ");
+              }
+                
+              if((mazeMap[row][col].wall&0x0001) == 0x0001)//if theres a rt wall
+                (void)printf("|");
+              else
+                (void)printf(" ");
+          }
+          (void)printf("\n");
+        }
+        
 }
       
 
